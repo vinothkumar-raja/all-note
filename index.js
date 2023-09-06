0fedb2c9811ca41e371bd4c08bbccd05861f1a19
Vinothkumar <68070917+vinothkumar-raja@users.noreply.github.com>
2023-09-06 17:17:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://all-note-app-default-rtdb.firebaseio.com/"
@@ -7,15 +7,31 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "todoList")
+const todoList = ref(database, "todoList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+onValue(todoList, function(list) {
+    let listArray = Objects.values(list.val())
+    shoppingListEl.innerHTML = ""
+    showList(listArray)
+})
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
     push(shoppingListInDB, inputValue)
-    
-    console.log(inputValue)
+    inputFieldEl.value = ""
 })
+
+function showList(listArray) {
+
+    let htmlList = ""
+    for(let i=0;i<listArray.length;i++) {
+        htmlList = `<li>${listArray[i]}</li>`
+    }
+
+    shoppingListEl.innerHTML = htmlList
+}
