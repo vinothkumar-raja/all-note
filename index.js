3ff14d3185d5718663b198f1d8eae2081c50e279
Vinothkumar <68070917+vinothkumar-raja@users.noreply.github.com>
2023-09-06 18:54:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function showList(listArray) {
         let currentId = currentList[0]
         let currentValue = currentList[1]
         let newEl = document.createElement("li")
-        newEl.textContent = itemValue
+        newEl.textContent = currentValue
         // Challenge: Attach an event listener to newEl and make it so you console log the id of the item when it's pressed.
         newEl.addEventListener("click", function() {
             console.log(currentId)
